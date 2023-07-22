a3ce8969e932245070984ac8f6270997bf53f87e
montio5 <moein@greenhabitat.at>
2023-07-22 11:38:03 +0330

    refactor: edit user logger

--- src/src/Listeners/UserLoggerListener.php
+++ src/src/Listeners/UserLoggerListener.php
@@ -2,48 +2,48 @@
 
 namespace App\Listeners;
 
-use App\Model\TimeLoggerInterface;
 use App\Model\UserLoggerInterface;
-use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Csrf\TokenStorage\TokenStorageInterface;
 
 class UserLoggerListener
 {
-    private $tokenStorage;
-    public function  __construct(TokenStorageInterface $tokenStorage){
-        $this->tokenStorage =$tokenStorage;
+    private TokenStorageInterface $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
     }
-    public function prePersist(LifecycleEventArgs $args): void
+
+    public function prePersist(LifecycleEventArgs $args)
     {
         $entity = $args->getObject();
 
-        // If this listener only applies to certain entity types,
-        // add some code to check the entity type as early as possible
         if (!$entity instanceof UserLoggerInterface) {
             return;
         }
-        $token =$this->tokenStorage->getToken();
-        $user = $token==null? null:$this->tokenStorage->getToken()->getUser();
-        $entity->setUpdatedUser($user);
+
+        $token = $this->tokenStorage->getToken();
+        $user = $token == null ? null : $this->tokenStorage->getToken()->getUser();
+
         $entity->setCreatedUser($user);
+        $entity->setUpdatedUser($user);
 
-
-        // ... do something with the entity
     }
 
-    public function preUpdate(PreUpdateEventArgs $args): void
+    public function preUpdate(PreUpdateEventArgs $args)
     {
         $entity = $args->getObject();
 
-        // If this listener only applies to certain entity types,
-        // add some code to check the entity type as early as possible
         if (!$entity instanceof UserLoggerInterface) {
             return;
         }
-        $token =$this->tokenStorage->getToken();
-        $user = $token==null? null:$this->tokenStorage->getToken()->getUser();
+
+        $token = $this->tokenStorage->getToken();
+        $user = $token == null ? null : $this->tokenStorage->getToken()->getUser();
+
         $entity->setUpdatedUser($user);
-         }
+    }
 }
