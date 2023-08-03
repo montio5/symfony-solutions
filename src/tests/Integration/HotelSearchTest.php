18b74d1ab52107de0645d632adbbdb4d9353321d
montio5 <moein@greenhabitat.at>
2023-08-03 21:04:13 +0330

    test: add test

--- src/tests/Integration/HotelSearchTest.php
+++ src/tests/Integration/HotelSearchTest.php
@@ -28,15 +28,15 @@ class HotelSearchTest extends KernelTestCase
 
 
         $hotel1 = new Hotel();
-        $hotel1->setName("abcd");
-        $hotel1->setAddress("some address");
+        $hotel1->setName("hotel1");
+        $hotel1->setAddress("address1");
         $hotel1->setPhone("654654");
         $hotel1->setEmail("test@gmail.com");
 
         $hotel2 = new Hotel();
-        $hotel2->setName("cdef");
-        $hotel2->setAddress("some address");
-        $hotel2->setPhone("654654");
+        $hotel2->setName("hotel2");
+        $hotel2->setAddress("address2");
+        $hotel2->setPhone("65465433");
         $hotel2->setEmail("admin@gmail.com");
 
         $hotelRepo->save($hotel1);
@@ -45,15 +45,15 @@ class HotelSearchTest extends KernelTestCase
         $hotelSearchService = static::getContainer()->get(SearchService::class);
         $this->assertInstanceOf(SearchService::class, $hotelSearchService);
 
-        $result = $hotelSearchService->search("ab");
+        $result = $hotelSearchService->search("1");
         $this->assertContains($hotel1, $result);
         $this->assertNotContains($hotel2, $result);
 
-        $result = $hotelSearchService->search("ef");
+        $result = $hotelSearchService->search("2");
         $this->assertContains($hotel2, $result);
         $this->assertNotContains($hotel1, $result);
 
-        $result = $hotelSearchService->search("cd");
+        $result = $hotelSearchService->search("hotel");
         $this->assertContains($hotel1, $result);
         $this->assertContains($hotel2, $result);
     }
