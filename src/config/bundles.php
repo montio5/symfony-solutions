9a313f9daacbd5f494066bb5c26ebcd06356f2dd
montio5 <90924916+montio5@users.noreply.github.com>
2023-08-04 18:10:01 +0330

    Revert "Assignment 7 (#3)" (#5)
    This reverts commit 931279ba2176fc798f5b7e07567e0c854762a205.

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -14,5 +14,4 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
