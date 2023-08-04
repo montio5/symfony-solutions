931279ba2176fc798f5b7e07567e0c854762a205
montio5 <90924916+montio5@users.noreply.github.com>
2023-08-04 18:02:02 +0330

    Assignment 7 (#3)
    * add admin panel
    
    * build: assignment 7
    
    * edit codes
    
    * fix: tests
    
    * test: add test
    
    * refactor: add some edits

--- src/src/Controller/HotelController.php
+++ src/src/Controller/HotelController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Controller;
-
 use App\Entity\Hotel;
 use App\Form\HotelType;
 use App\Hotel\SearchService;
@@ -10,21 +8,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/hotel')]
+
+#[Route('/{_locale}/hotel')]
 class HotelController extends AbstractController
 {
     #[Route('/search', name: 'app_hotel_search', methods: ['GET'])]
-    public function search(Request $request, SearchService $hotelSearchService): Response {
+    #[IsGranted("ROLE_USER")]
+    public function search(Request $request, SearchService $hotelSearchService): Response
+    {
         $query = $request->query->get('q');
 
         return $this->render('hotel/index.html.twig', [
-            'q'      => $query,
+            'q' => $query,
             'hotels' => $hotelSearchService->search($query),
         ]);
     }
 
     #[Route('/', name: 'app_hotel_index', methods: ['GET'])]
+    #[IsGranted("ROLE_USER")]
     public function index(HotelRepository $hotelRepository): Response
     {
         return $this->render('hotel/index.html.twig', [
@@ -33,18 +36,16 @@ class HotelController extends AbstractController
     }
 
     #[Route('/new', name: 'app_hotel_new', methods: ['GET', 'POST'])]
+    #[IsGranted("ROLE_HOTEL_OWNER")]
     public function new(Request $request, HotelRepository $hotelRepository): Response
     {
         $hotel = new Hotel();
         $form = $this->createForm(HotelType::class, $hotel);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
             $hotelRepository->save($hotel, true);
-
             return $this->redirectToRoute('app_hotel_index', [], Response::HTTP_SEE_OTHER);
         }
-
         return $this->renderForm('hotel/new.html.twig', [
             'hotel' => $hotel,
             'form' => $form,
@@ -52,6 +53,7 @@ class HotelController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_hotel_show', methods: ['GET'])]
+    #[IsGranted("VIEW", "hotel")]
     public function show(Hotel $hotel): Response
     {
         return $this->render('hotel/show.html.twig', [
@@ -60,17 +62,16 @@ class HotelController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_hotel_edit', methods: ['GET', 'POST'])]
+    //    #[IsGranted("ROLE_HOTEL_OWNER")]
+    #[IsGranted("EDIT", "hotel")]
     public function edit(Request $request, Hotel $hotel, HotelRepository $hotelRepository): Response
     {
         $form = $this->createForm(HotelType::class, $hotel);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
             $hotelRepository->save($hotel, true);
-
             return $this->redirectToRoute('app_hotel_index', [], Response::HTTP_SEE_OTHER);
         }
-
         return $this->renderForm('hotel/edit.html.twig', [
             'hotel' => $hotel,
             'form' => $form,
@@ -78,9 +79,10 @@ class HotelController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_hotel_delete', methods: ['POST'])]
+    #[IsGranted("DELETE", "hotel")]
     public function delete(Request $request, Hotel $hotel, HotelRepository $hotelRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$hotel->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $hotel->getId(), $request->request->get('_token'))) {
             $hotelRepository->remove($hotel, true);
         }
 
