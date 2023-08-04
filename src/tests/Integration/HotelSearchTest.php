12c209821491c45d7db487e3498473d3be1f0837
montio5 <90924916+montio5@users.noreply.github.com>
2023-08-04 18:01:38 +0330

    Assignment 6 (#4)
    * edit codes
    
    * fix: tests
    
    * test: add test

--- src/tests/Integration/HotelSearchTest.php
+++ src/tests/Integration/HotelSearchTest.php
@@ -4,11 +4,13 @@ namespace App\Tests\Integration;
 
 use App\Entity\Hotel;
 use App\Hotel\SearchService;
+use App\Repository\HotelRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class HotelSearchTest extends KernelTestCase
 {
+
     protected static function getKernelClass(): string
     {
         return \App\Kernel::class;
@@ -19,16 +21,23 @@ class HotelSearchTest extends KernelTestCase
         $kernel = self::bootKernel();
         $this->assertSame('test', $kernel->getEnvironment());
 
+
+        /** @var EntityManagerInterface $entityManager */
         $entityManager = static::getContainer()->get(EntityManagerInterface::class);
         $hotelRepo = $entityManager->getRepository(Hotel::class);
 
+
         $hotel1 = new Hotel();
-        $hotel1->setName("abcd");
-        $hotel1->setAddress("some address");
+        $hotel1->setName("hotel1");
+        $hotel1->setAddress("address1");
+        $hotel1->setPhone("654654");
+        $hotel1->setEmail("test@gmail.com");
 
         $hotel2 = new Hotel();
-        $hotel2->setName("cdef");
-        $hotel2->setAddress("some address");
+        $hotel2->setName("hotel2");
+        $hotel2->setAddress("address2");
+        $hotel2->setPhone("65465433");
+        $hotel2->setEmail("admin@gmail.com");
 
         $hotelRepo->save($hotel1);
         $hotelRepo->save($hotel2, true);
@@ -36,13 +45,15 @@ class HotelSearchTest extends KernelTestCase
         $hotelSearchService = static::getContainer()->get(SearchService::class);
         $this->assertInstanceOf(SearchService::class, $hotelSearchService);
 
-        $result = $hotelSearchService->search("ab");
+        $result = $hotelSearchService->search("1");
         $this->assertContains($hotel1, $result);
+        $this->assertNotContains($hotel2, $result);
 
-        $result = $hotelSearchService->search("ef");
+        $result = $hotelSearchService->search("2");
         $this->assertContains($hotel2, $result);
+        $this->assertNotContains($hotel1, $result);
 
-        $result = $hotelSearchService->search("cd");
+        $result = $hotelSearchService->search("hotel");
         $this->assertContains($hotel1, $result);
         $this->assertContains($hotel2, $result);
     }
